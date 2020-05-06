59ae15fb614fde5adb505f5216a2b641ce508cf1
yangjiayi <yang_kate@126.com>
2020-05-06 23:15:48 +0800

    【ADD】启动application

---
+++ seckill-admin/seckill-admin-service/src/main/java/com/dubboss/seckilladminservice/SeckillAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dubboss.seckilladminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckillAdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeckillAdminServiceApplication.class, args);
+    }
+
+}
