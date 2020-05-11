08faa888dc9d1ab20d941c254d5d6f167772e8f2
yangjiayi <yang_kate@126.com>
2020-05-11 23:03:42 +0800

    【ADD】两次MD5加密密码

--- seckill-system-springboot/seckill-provider/src/main/java/com/dubboss/seckillprovider/redis/KeyPrefix.java
+++ seckill-system-springboot/seckill-provider/src/main/java/com/dubboss/seckillprovider/redis/KeyPrefix.java
@@ -9,7 +9,21 @@ package com.dubboss.seckillprovider.redis;
  **/
 public interface KeyPrefix {
 
+    /**
+     * @param
+     * @return int
+     * @Author yangjiayi
+     * @Description //过期时间
+     * @Date 22:32 2020/5/11
+     */
     public int expireSeconds();
 
+    /**
+     * @param
+     * @return java.lang.String
+     * @Author yangjiayi
+     * @Description //key前缀
+     * @Date 22:33 2020/5/11
+     */
     public String getPrefix();
 }
